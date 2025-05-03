16e13ebe1613b11aa0dcd1f851de459bbb44cc3f
Thiago Reis <thiagomreeis@gmail.com>
2025-05-03 15:51:39 -0300

    feat: Building Carousel component

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,14 +1,63 @@
 import Card from "../Card/Card";
 import cars from "../../../public/api/cars.json";
 import { Car } from "../../interfaces/Car";
+import {
+  useSnapIndicators,
+  useSnapNavigation,
+} from "@volvo-cars/react-headless";
+import { IconButton } from "@volvo-cars/react-icons";
+import { useRef } from "react";
 
 export default function Carousel() {
+  const ref = useRef(null);
+  const { previousButtonProps, nextButtonProps } = useSnapNavigation({
+    ref,
+  });
+  const { indicators } = useSnapIndicators({
+    ref,
+  });
   return (
-    <div className="container-lg flex-row flex-wrap">
-      {cars.map((car) => (
-        <Card key={car.id} car={car as Car} />
-      ))}
-      ;
-    </div>
+    <section className="container-lg  py-s" data-bleed="until-md">
+      <div ref={ref} className="reel scrollbar-none ">
+        {cars.map((car, index) => (
+          <div
+            id={car.id}
+            className="snap-center transition-colors"
+            key={index}
+            style={{
+              width: "25%",
+              flex: "0 0 25%",
+              marginInlineStart: index === 0 ? "0%" : undefined,
+              marginInlineEnd: index === cars.length - 1 ? "0%" : undefined,
+            }}
+          >
+            <Card key={car.id} car={car as Car} />
+          </div>
+        ))}
+      </div>
+      <ul className="pagination-dots md:hidden lg:hidden">
+        {indicators.map((props, index) => {
+          return (
+            <li key={index}>
+              <button {...props}>
+                <span className="sr-only">Go to next car {index + 1}</span>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="container-sm flex gap-16 mt-24 justify-end mr-0">
+        <IconButton
+          icon="chevron-back"
+          variant="outlined"
+          {...previousButtonProps}
+        />
+        <IconButton
+          icon="chevron-forward"
+          variant="outlined"
+          {...nextButtonProps}
+        />
+      </div>
+    </section>
   );
 }
